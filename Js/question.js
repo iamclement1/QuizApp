3bddc40d7b32456a027c0a357721dbb32d2e904f
iamclement1 <blessedmarcel1@gmail.com>
2021-01-26 21:39:01 +0100

    my first commit

---
+++ Js/question.js
@@ -0,0 +1,108 @@
+function Quiz(questions) {
+  this.score = 0;
+  this.questions = questions;
+  this.questionIndex = 0;
+}
+
+Quiz.prototype.getQuestionIndex = function () {
+  return this.questions[this.questionIndex];
+};
+
+Quiz.prototype.guess = function (answer) {
+  if (this.getQuestionIndex().isCorrectAnswer(answer)) {
+    this.score++;
+  }
+
+  this.questionIndex++;
+};
+
+Quiz.prototype.isEnded = function () {
+  return this.questionIndex === this.questions.length;
+};
+
+function Question(text, choices, answer) {
+  this.text = text;
+  this.choices = choices;
+  this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function (choice) {
+  return this.answer === choice;
+};
+
+function populate() {
+  if (quiz.isEnded()) {
+    showScores();
+  } else {
+    // show question
+    var element = document.getElementById('question');
+    element.innerHTML = quiz.getQuestionIndex().text;
+
+    // show options
+    var choices = quiz.getQuestionIndex().choices;
+    for (var i = 0; i < choices.length; i++) {
+      var element = document.getElementById('choice' + i);
+      element.innerHTML = choices[i];
+      guess('btn' + i, choices[i]);
+    }
+
+    showProgress();
+  }
+}
+
+function guess(id, guess) {
+  var button = document.getElementById(id);
+  button.onclick = function () {
+    quiz.guess(guess);
+    populate();
+  };
+}
+
+function showProgress() {
+  var currentQuestionNumber = quiz.questionIndex + 1;
+  var element = document.getElementById('progress');
+  element.innerHTML =
+    'Question ' + currentQuestionNumber + ' of ' + quiz.questions.length;
+}
+
+function showScores() {
+  var gameOverHTML = '<h1>Result</h1>';
+  gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + '</h2>';
+  var element = document.getElementById('quiz');
+  element.innerHTML = gameOverHTML;
+}
+
+// create questions here
+var questions = [
+  new Question(
+    'Adedeji Stephen is another known as...?',
+    ['A Bastard', 'A pari to n ko code', 'Ionware', 'All'],
+    'All'
+  ),
+  new Question(
+    'The most paranoid man that has ever lived is?',
+    ['Ogunshina Pelumi', 'Jimoh Lawal', 'Obisesan Oluseye', 'Adeyemo Habeeb'],
+    'Ogunshina Pelumi'
+  ),
+  new Question(
+    'JK is also known as ...',
+    ['His Majesty', 'The talkative', 'The Wise One', 'The Mentor'],
+    'The Wise One'
+  ),
+  new Question(
+    'Adminstrator is synonymous to ...?',
+    ['A Bully', 'The Owner', 'The Manager', 'All'],
+    'The Manager'
+  ),
+  new Question(
+    'If you never believe, you can never ...',
+    ['Understand', 'Grab it', 'Achieve', 'Make it'],
+    'Achieve'
+  ),
+];
+
+// create quiz
+var quiz = new Quiz(questions);
+
+// display quiz
+populate();
